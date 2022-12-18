3d64614b086c7e4c246142e752ffab8491781d9d
thegreenpaladin <62639821+thegreenpaladin@users.noreply.github.com>
2022-12-18 21:00:19 +0500

    Login Page with Bootstrap

--- luna-frontend/src/Login.js
+++ luna-frontend/src/Login.js
@@ -11,23 +11,29 @@ const Login = () => {
     }
     
     return (
-        <div className="bg">
-        <form onSubmit={handleSubmit}>
-            <h1 className="text-white">Login</h1>
-            <div>
-                <label className="text-white">
-                    Username:
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </label>
+        <div className='bg'>
+        <div className="row h-100 justify-content-center align-items-center">
+            <div className="col-3">
+                <form onSubmit={handleSubmit}>
+                    <h1 className='text-white text-center'>Login</h1>
+                    <div className="form-group">
+                        <label htmlFor="username" className='text-white'>Username</label>
+                        <input type="text" className="form-control" id="username" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password" className='text-white'>Password</label>
+                        <input type="password" className="form-control" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="rememberMe" />
+                        <label className="form-check-label text-white" htmlFor="rememberMe">Remember me</label>
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label className="text-white">
-                    Password:
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-            </div>
-            <input type="submit" className="btn btn-primary" value="Login" />
-            </form>
+        </div>
         </div>
     )
 }
