a03d860bd7255c9abee22e92f91109db371f78e1
Hammad Ahmed <62638943+HunterTravis@users.noreply.github.com>
2022-12-30 00:54:10 +0500

    courses css fix

--- luna-frontend/src/Pages/Courses/Courses.js
+++ luna-frontend/src/Pages/Courses/Courses.js
@@ -19,17 +19,124 @@ const dummyData = [
       { id: 4, name: 'Assignment 4' }
     ]
   },
-  // ... additional courses
+  {
+    id: 3,
+    courseName: 'Course 3',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 4,
+    courseName: 'Course 4',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 5,
+    courseName: 'Course 5',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 6,
+    courseName: 'Course 6',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 7,
+    courseName: 'Course 7',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 8,
+    courseName: 'Course 8',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 9,
+    courseName: 'Course 9',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 11,
+    courseName: 'Course 11',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 12,
+    courseName: 'Course 12',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 13,
+    courseName: 'Course 13',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 14,
+    courseName: 'Course 14',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 15,
+    courseName: 'Course 15',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
+  {
+    id: 16,
+    courseName: 'Course 16',
+    assignments: [
+      { id: 3, name: 'Assignment 3' },
+      { id: 4, name: 'Assignment 4' }
+    ]
+  },
 ];
 
-function Courses() {
+function Courses(props) {
+  const data = dummyData;
   return (
     <div className="courses">
-      <h1>Courses</h1>
-      <div className="course-cards">
-        {dummyData.map(course => (
-          <CourseCard key={course.id} {...course} />
-        ))}
+      <div className="container">
+
+        <h1>Courses</h1>
+        <div className="course-cards mr-0">
+          {data.map(course => (
+            <CourseCard key={course.id} {...course} />
+          ))}
+        </div>
       </div>
     </div>
   );
