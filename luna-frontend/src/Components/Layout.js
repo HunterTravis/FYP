30fdf42de7a33b1d5b6d219b1f7b3dbd088c418a
thegreenpaladin <62639821+thegreenpaladin@users.noreply.github.com>
2022-12-30 00:26:12 +0500

    Fixed Navbar

--- luna-frontend/src/Components/Layout.js
+++ luna-frontend/src/Components/Layout.js
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="sidebar-layout">
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="lower">
         <Sidebar />
         <Outlet />
