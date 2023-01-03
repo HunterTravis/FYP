14fd87b2f8e72c176815cdddc023074da6d27f74
thegreenpaladin <62639821+thegreenpaladin@users.noreply.github.com>
2023-01-04 01:41:25 +0500

    Changed radio buttons

--- luna-frontend/src/Pages/Marks.js
+++ luna-frontend/src/Pages/Marks.js
@@ -1,16 +1,31 @@
 import MarksCard from "../Components/MarksCard";
+import { useState } from "react";
+
 
 const Marks = () => {
+
+  const [arr, setArr] = useState([1, 0, 0]);
+  const onClick = (e) => {
+    setArr(e === 1? 0 : (e === 2 ? 1 : 2));
+  }
+
   return (
     <div className="marks">
       <div className="container">
         <h1>Marks</h1>
         <div className="sem-selector text-center">
-          <select name="sem" id="sem-select">
-            <option value="fall-22">Fall 2022</option>
-            <option value="summer-22">Summer 2022</option>
-            <option value="spring-22">Spring 2022</option>
-          </select>
+          <label htmlFor="fall-22">
+            <input type="radio" id="fall-22" name="sem" value="fall22" defaultChecked />
+            Fall 2022
+          </label>
+          <label htmlFor="summer-22">
+            <input type="radio" id="summer-22" name="sem" value="summer22" />
+            Summer 2022
+          </label>
+          <label htmlFor="spring-22">
+            <input type="radio" id="spring-22" name="sem" value="spring22" />
+            Spring 2022
+          </label>
         </div>
         <div className="marks-card-display">
           <MarksCard />
