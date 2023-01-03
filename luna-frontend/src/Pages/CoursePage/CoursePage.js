b858450931a356f07c0fe0134aedd344b5c496ec
thegreenpaladin <62639821+thegreenpaladin@users.noreply.github.com>
2023-01-03 19:20:06 +0500

    Made course content tab pane

--- luna-frontend/src/Pages/CoursePage/CoursePage.js
+++ luna-frontend/src/Pages/CoursePage/CoursePage.js
@@ -1,49 +1,181 @@
-import React from 'react';
-import './CoursePage.css';
-import { useLocation } from 'react-router-dom';
-const CoursePage = ({ courseName, announcements, assignments, events, resources }) => {
+import React from "react";
+import "./CoursePage.css";
+import { useLocation } from "react-router-dom";
+const CoursePage = () => {
   const location = useLocation();
   const { courseHead } = location.state;
 
+  const announcements = [
+    {
+      id: 1,
+      title: "Announcement 1",
+      description: "This is the first announcement",
+    },
+    {
+      id: 2,
+      title: "Announcement 2",
+      description: "This is the second announcement",
+    },
+  ];
+
+  const assignments = [
+    {
+      id: 1,
+      title: "Assignment 1",
+      description: "This is the first assignment",
+    },
+    {
+      id: 2,
+      title: "Assignment 2",
+      description: "This is the second assignment",
+    },
+  ];
+
+  const resources = [
+    {
+      id: 1,
+      title: "Resource 1",
+      description: "This is the first resource",
+    },
+    {
+      id: 2,
+      title: "Resource 2",
+      description: "This is the second resource",
+    },
+  ];
 
   return (
-    <div>
-      {/* Course name */}
-      <h1>{courseHead}</h1>
-
-      
-      {/* <h2>Announcements</h2>
-      <ul>
-        {announcements.slice(0, 3).map((announcement) => (
-          <li>{announcement}</li>
-        ))}
-      </ul>
-
-      
-      <h2>Assignments</h2>
-      <ul>
-        {assignments.slice(0, 3).map((assignment) => (
-          <li>{assignment}</li>
-        ))}
-      </ul>
-
-      
-      <h2>Course Stream</h2>
-      <ul>
-        {events.map((event) => (
-          <li>{event}</li>
-        ))}
-      </ul>
-
-      
-      <div style={{ float: 'right', width: '30%' }}>
-        <h2>Resources</h2>
-        <ul>
-          {resources.slice(0, 3).map((resource) => (
-            <li>{resource}</li>
-          ))}
-        </ul>
-      </div> */}
+    <div className="course-page">
+      <div className="container">
+        {/* Course name */}
+        <h1>{courseHead}</h1>
+        <div className="course-page__content-list">
+          <ul className="nav nav-tabs" role="tablist">
+            <li className="nav-item" role="presentation">
+              <a
+                className="nav-link active"
+                id="course-stream"
+                data-bs-toggle="tab"
+                href="#Stream"
+                role="tab"
+                aria-controls="Stream"
+                aria-selected="true"
+              >
+                Course Stream
+              </a>
+            </li>
+            <li className="nav-item" role="presentation">
+              <a
+                className="nav-link"
+                id="announcements"
+                data-bs-toggle="tab"
+                href="#Announcements"
+                role="tab"
+                aria-controls="Announcements"
+                aria-selected="false"
+              >
+                Announcements
+              </a>
+            </li>
+            <li className="nav-item" role="presentation">
+              <a
+                className="nav-link"
+                id="assignments"
+                data-bs-toggle="tab"
+                href="#Assignments"
+                role="tab"
+                aria-controls="Assignments"
+                aria-selected="false"
+              >
+                Assignments
+              </a>
+            </li>
+            <li className="nav-item" role="presentation">
+              <a
+                className="nav-link"
+                id="resources"
+                data-bs-toggle="tab"
+                href="#Resources"
+                role="tab"
+                aria-controls="Resources"
+                aria-selected="false"
+              >
+                Resources
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="tab-content" id="myTabContent">
+          <div className="tab-pane show active" id="Stream" role="tabpanel">
+            <ul>
+              {announcements.map((announcement) => (
+                <li>
+                  <div>
+                    <h5>{announcement.title}</h5>
+                    <p>{announcement.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <ul>
+              {assignments.map((assignment) => (
+                <li>
+                  <div>
+                    <h5>{assignment.title}</h5>
+                    <p>{assignment.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <ul>
+              {resources.map((resource) => (
+                <li>
+                  <div>
+                    <h5>{resource.title}</h5>
+                    <p>{resource.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tab-pane fade" id="Announcements" role="tabpanel">
+            <ul>
+              {announcements.map((announcement) => (
+                <li>
+                  <div>
+                    <h5>{announcement.title}</h5>
+                    <p>{announcement.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tab-pane fade" id="Assignments" role="tabpanel">
+            <ul>
+              {assignments.map((assignment) => (
+                <li>
+                  <div>
+                    <h5>{assignment.title}</h5>
+                    <p>{assignment.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tab-pane fade" id="Resources" role="tabpanel">
+            <ul>
+              {resources.map((resource) => (
+                <li>
+                  <div>
+                    <h5>{resource.title}</h5>
+                    <p>{resource.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
