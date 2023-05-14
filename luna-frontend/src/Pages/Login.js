d08aa1d827884e929d08a32db4531f7a31d483fc
Muhammad Ahsan <ahsanmuhammed45@gmail.com>
2023-05-15 00:16:06 +0500

    Set up login nav to Teacher

--- luna-frontend/src/Pages/Login.js
+++ luna-frontend/src/Pages/Login.js
@@ -1,70 +1,114 @@
-import './Login.css';
-import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import { useCookies } from 'react-cookie';
+import "./Login.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [cookies, setCookie] = useCookies(['user']);
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cookies, setCookie] = useCookies(["user"]);
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetch('http://localhost:3001/auth/login', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username,password})
-        })
-        .then(response => response.json())
-        .then(data => {
-            if(data.status===200){
-                //set cookie
-                setCookie('username', username, { path: '/' });
-                setCookie('LoggedIn', "true", { path: '/' });
-                navigate(data.navigation);
-            }
-            else
-            {
-                alert(data.message);
-            }
-        })
-        .catch(error => {alert("Something went wrong"); console.log(error)});
-    }
-    
-    return (
-        <div className="bg">
-            <div className="row h-100 justify-content-center">
-                <div id="loginPageContainer" className="col-lg-3 col-9">
-                    <img src="images/logo.png" alt="Could not load logo" className="logo"/>
-                    <h1 className="text-white login text-center">Login</h1>
-                    <form onSubmit={handleSubmit} className="loginForm">
-                        <div className="form-group">
-                            <label htmlFor="InputEmail" className="text-white">Username or Roll No:</label>
-                            <input type="text" className="form-control"  id="InputEmail" placeholder="Enter username or roll number" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                        </div>
-                        <div className="form-group">
-                                <label htmlFor="InputPassword" className="text-white  mt-2">Password:</label>
-                            <input type="password" className="form-control" id="InputPassword" placeholder="********" value={password}  onChange={(e)=> setPassword(e.target.value)}/>
-                        </div>
-                        <div className="form-check">
-                            <input type="checkbox" className="form-check-input mt-2" id="RememberMe"/>
-                            <label className="form-check-label text-white mt-1" htmlFor="RememberMe">Remember Me</label>
-                        </div>
-                        <div className="form-check">
-                            <input type="checkbox" className="form-check-input mt-2" id="forgot"/>
-                            <label className="form-check-label text-white mt-1" htmlFor="forgot">Forget Password</label>
-                        </div>
-                        <div className="text-center">
-                            <button type="submit" className="btn btn-primary w-50 mt-3">Login</button>
-                        </div>
-                    </form>
-                </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 200) {
+          //set cookie
+          setCookie("username", username, { path: "/" });
+          setCookie("LoggedIn", "true", { path: "/" });
+          console.log(data.navigation);
+          navigate(data.navigation);
+        } else {
+          alert(data.message);
+        }
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="bg">
+      <div className="row h-100 justify-content-center">
+        <div id="loginPageContainer" className="col-lg-3 col-9">
+          <img
+            src="images/logo.png"
+            alt="Could not load logo"
+            className="logo"
+          />
+          <h1 className="text-white login text-center">Login</h1>
+          <form onSubmit={handleSubmit} className="loginForm">
+            <div className="form-group">
+              <label htmlFor="InputEmail" className="text-white">
+                Username or Roll No:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="InputEmail"
+                placeholder="Enter username or roll number"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
+            <div className="form-group">
+              <label htmlFor="InputPassword" className="text-white  mt-2">
+                Password:
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="InputPassword"
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input mt-2"
+                id="RememberMe"
+              />
+              <label
+                className="form-check-label text-white mt-1"
+                htmlFor="RememberMe"
+              >
+                Remember Me
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input mt-2"
+                id="forgot"
+              />
+              <label
+                className="form-check-label text-white mt-1"
+                htmlFor="forgot"
+              >
+                Forget Password
+              </label>
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary w-50 mt-3">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
