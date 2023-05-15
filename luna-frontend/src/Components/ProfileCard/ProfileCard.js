04e1e105301c354ad81beb0c707c28255bae96f1
Muhammad Ahsan <ahsanmuhammed45@gmail.com>
2023-05-15 22:48:52 +0500

    Several small tasks

--- luna-frontend/src/Components/ProfileCard/ProfileCard.js
+++ luna-frontend/src/Components/ProfileCard/ProfileCard.js
@@ -6,58 +6,91 @@ function ProfileCard({ student }) {
 
   return (
     <div className="profile-card">
-      <div className="uni-info">
+      <div className="info">
         <div className="info-title">
           <h2>University Information</h2>
         </div>
         <div className="info-body">
-          <p>Roll No: {mockStudentData.UniversityInformation.RollNumber}</p>
-          <p>Degree: {mockStudentData.UniversityInformation.Degree}</p>
-          <p>Batch: {mockStudentData.UniversityInformation.Batch}</p>
-          <p>Section: {mockStudentData.UniversityInformation.Section} </p>
-          <p>Campus: {mockStudentData.UniversityInformation.Campus}</p>
-          <p>Status: {mockStudentData.UniversityInformation.Status}</p>
+          <table>
+            <tr>
+              <td>
+                Roll No: {mockStudentData.UniversityInformation.RollNumber}
+              </td>
+              <td>Degree: {mockStudentData.UniversityInformation.Degree}</td>
+            </tr>
+            <tr>
+              <td>Batch: {mockStudentData.UniversityInformation.Batch}</td>
+              <td>Section: {mockStudentData.UniversityInformation.Section}</td>
+            </tr>
+            <tr>
+              <td>Campus: {mockStudentData.UniversityInformation.Campus}</td>
+              <td>Status: {mockStudentData.UniversityInformation.Status}</td>
+            </tr>
+          </table>
         </div>
       </div>
 
-      <div className="personal-info">
+      <div className="info">
         <div className="info-title">
           <h2>Personal Information</h2>
         </div>
         <div className="info-body">
-          <p>Name: {mockStudentData.PersonalInformation.Name}</p>
-          <p>Gender: {mockStudentData.PersonalInformation.Gender}</p>
-          <p>DOB: {mockStudentData.PersonalInformation.DOB}</p>
-          <p>CNIC: {mockStudentData.PersonalInformation.CNIC}</p>
-          <p>Email: {mockStudentData.PersonalInformation.Email}</p>
-          <p>Mobile No: {mockStudentData.PersonalInformation.MobileNumber}</p>
+          <table>
+            <tr>
+              <td>Name: {mockStudentData.PersonalInformation.Name}</td>
+              <td>CNIC: {mockStudentData.PersonalInformation.CNIC}</td>
+            </tr>
+            <tr>
+              <td>Gender: {mockStudentData.PersonalInformation.Gender}</td>
+              <td>Email: {mockStudentData.PersonalInformation.Email}</td>
+            </tr>
+            <tr>
+              <td>DOB: {mockStudentData.PersonalInformation.DOB}</td>
+              <td>
+                Mobile No: {mockStudentData.PersonalInformation.MobileNumber}
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
 
-      <div className="contact-info">
+      <div className="info">
         <div className="info-title">
           <h2>Contact Information</h2>
         </div>
         <div className="info-body">
-          <p>Address: {mockStudentData.ContactInformation.Address}</p>
-          <p>Postal code: {mockStudentData.ContactInformation.PostalCode}</p>
-          <p>City: {mockStudentData.ContactInformation.City}</p>
-          <p>Country: {mockStudentData.ContactInformation.Country}</p>
-          <p>Contact No: {mockStudentData.ContactInformation.ContactNumber}</p>
+          <table>
+            <tr>
+              <td>Address: {mockStudentData.ContactInformation.Address}</td>
+              <td>
+                Postal code: {mockStudentData.ContactInformation.PostalCode}
+              </td>
+            </tr>
+            <tr>
+              <td>City: {mockStudentData.ContactInformation.City}</td>
+              <td>
+                Contact No: {mockStudentData.ContactInformation.ContactNumber}
+              </td>
+            </tr>
+            <tr>
+              <td>Country: {mockStudentData.ContactInformation.Country}</td>
+              <td></td>
+            </tr>
+          </table>
         </div>
       </div>
 
-      <div className="family-info">
+      <div className="info">
         <div className="info-title">
           <h2>Family Information</h2>
         </div>
-        <div className="info-body">
-          <div>
+        <div className="info-body d-flex">
+          <div className="father">
             <p>Name: {mockStudentData.FamilyInformation[0].Name}</p>
             <p>Relation: {mockStudentData.FamilyInformation[0].Relation}</p>
             <p>CNIC: {mockStudentData.FamilyInformation[0].CNIC}</p>
           </div>
-          <div>
+          <div className="mother">
             <p>Name: {mockStudentData.FamilyInformation[1].Name}</p>
             <p>Relation: {mockStudentData.FamilyInformation[1].Relation}</p>
             <p>CNIC: {mockStudentData.FamilyInformation[1].CNIC}</p>
